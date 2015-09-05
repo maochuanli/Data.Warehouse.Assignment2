96a63a33c2cf8a20e15b0c3ce863a29f7cd76a25
Mao Chuan Li <mao.chuan.li@gmail.com>
2015-09-05 12:25:11 +1200

    Fix the oracle script error
    using rownum for Oracle database, and append the ';' characer at
    the end of each line
    Signed-off-by: Mao Chuan Li <mao.chuan.li@gmail.com>

--- DDL/Assign2.DW.Query.Oracle.sql
+++ DDL/Assign2.DW.Query.Oracle.sql
@@ -1,33 +1,33 @@
 
 --1, Which product generated maximum sales in Dec, 2014?
-select p.product_name, sum(s.TOTAL_SALE) as product_total_sale from sales as s,products as p,dates as d
-    where d.mm=12 and
-          d.DATE_ID = s.DATE_ID and
-          p.PRODUCT_ID = s.PRODUCT_ID and
-          rownum <= 3
-          group by p.product_name 
-          order by product_total_sale desc
-          ;
+select * from  (  
+  select products.product_name, sum(sales.TOTAL_SALE) as product_total_sale from sales, products, dates
+    where dates.mm=12 and
+          dates.DATE_ID = sales.DATE_ID and
+          products.PRODUCT_ID = sales.PRODUCT_ID
+          group by products.product_name 
+          order by product_total_sale desc          
+    
+) where ROWNUM <= 3;
 
 --2, Which store produced highest sales in the whole year?
-select s.STORE_NAME, sum(sales.TOTAL_SALE) store_sales from stores s, sales, dates d where 
+select * from  (  
+  select s.STORE_NAME, sum(sales.TOTAL_SALE) store_sales from stores s, sales, dates d where 
     s.STORE_ID = sales.STORE_ID and 
     sales.DATE_ID = d.DATE_ID and
-    d.YYYY = 2014 and
-    rownum <= 3
+    d.YYYY = 2014 
     group by s.STORE_NAME
-    ;
-
+    
+) where ROWNUM <= 3;
 --3, Determine the supplier name for the most popular product based on sales.
-select s.SUPPLIER_NAME, sum(sales.TOTAL_SALE) supplier_sales from suppliers s, sales, products p where
+select * from  (  
+  select s.SUPPLIER_NAME, sum(sales.TOTAL_SALE) supplier_sales from suppliers s, sales, products p where
     sales.SUPPLIER_ID = s.SUPPLIER_ID and 
-    p.PRODUCT_ID = sales.PRODUCT_ID and
-    rownum <= 3
+    p.PRODUCT_ID = sales.PRODUCT_ID     
     group by SUPPLIER_NAME
-    ;
+    
+) where ROWNUM <= 3;
 
 --4, Presents the quarterly sales analysis for all stores using drill down query concepts.
 
 --5, Create a materialised view with name "STOREANALYSIS" that present the product-wise sales analysis for each store.
-
-;
